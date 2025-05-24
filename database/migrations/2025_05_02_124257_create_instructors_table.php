b2c386736dfe54c15d4fd3cb9134fb52b3e2d6d7
sadique-cws <sadique.hussain96@gmail.com>
2025-05-24 14:07:07 +0530

    Update instructors migration to add foreign key constraint for user_id

--- database/migrations/2025_05_02_124257_create_instructors_table.php
+++ database/migrations/2025_05_02_124257_create_instructors_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('instructors', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->foreignId("user_id")->constrained()->onDelete("cascade");
             $table->string('email')->unique();
             $table->string('contact')->nullable();
             $table->boolean('status')->default(0);
